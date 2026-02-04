993bdb73ca092cc7c0bf4f8fa91053932598f4ce
Vishal R <vishal.r2023c@vitstudent.ac.in>
2026-02-04 20:56:40 +0530

    To test the failure case for task 15

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins! Task 7 is successful.");
+        System.out.println("Hello Jenkins! Task 7 is successful.")
     }
 }
